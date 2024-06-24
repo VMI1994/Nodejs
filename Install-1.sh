09b304a9dadb1b69371cd87e8812c0bce4b719b4
Just Playin <ensminger@yahoo.com>
2024-06-24 12:53:53 -0400

    Update and rename Install.sh to Install-1.sh

---
+++ Install-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clear
+echo "We will now install node.js"
+# Install node.js
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+exec bash
