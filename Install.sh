0489243c1c90444c175599fec0e205ec8bf2b6b1
Just Playin <ensminger@yahoo.com>
2024-06-24 13:31:59 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 clear
+echo "Which version of Nodejs would you like to install?"
+read version
+echo
 echo "We will now install node.js"
-# Install node.js
+
 # installs nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-sleep 2
-export NVM_DIR="$HOME/.nvm" 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sleep 2
+source ~/.bashrc
+sleep 1
 
+# Install node.js
 # download and install Node.js (you may need to restart the terminal)
-nvm install 22
+nvm install $version
 
 echo "PM2 Process manager will now be installed"
 sleep 2
 npm install pm2@latest -g
+sleep 2
 cmd=$(pm2 startup | grep sudo)
-sudo $cmd
+echo $cmd | bash
 exit
