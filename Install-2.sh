4b4ab5d5ce988acd9e59859c854b87b0d9027b9c
Just Playin <ensminger@yahoo.com>
2024-06-24 12:54:38 -0400

    Create Install-2.sh

---
+++ Install-2.sh
@@ -0,0 +1,9 @@
+# download and install Node.js (you may need to restart the terminal)
+nvm install 18
+
+echo "PM2 Process manager will now be installed"
+sleep 2
+npm install pm2@latest -g
+cmd=$(pm2 startup | grep sudo)
+sudo $cmd
+exit
