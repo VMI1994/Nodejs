2a695663af7f3208d78b5595455579eb348286f3
Just Playin <ensminger@yahoo.com>
2024-06-24 11:03:16 -0400

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo "We will now install node.js"
+sleep 2
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt update
+sudo apt install -y nodejs
+sudo apt install -y npm
