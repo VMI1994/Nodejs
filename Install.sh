ca6ce0939cbe52511c29c60f04b7934cced879e1
Just Playin <ensminger@yahoo.com>
2024-06-24 13:06:04 -0400

    Update and rename Install-1.sh to Install.sh

--- Install.sh
+++ Install.sh
@@ -8,4 +8,13 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 sleep 2
 source ~/.bashrc
 sleep 2
+
+# download and install Node.js (you may need to restart the terminal)
+nvm install 22
+
+echo "PM2 Process manager will now be installed"
+sleep 2
+npm install pm2@latest -g
+cmd=$(pm2 startup | grep sudo)
+sudo $cmd
 exit
