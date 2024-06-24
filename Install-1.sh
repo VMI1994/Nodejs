ea1f523d61f721ae1495a4f5808eb527101b7a3c
Just Playin <ensminger@yahoo.com>
2024-06-24 13:04:51 -0400

    Update Install-1.sh

--- Install-1.sh
+++ Install-1.sh
@@ -5,4 +5,7 @@ echo "We will now install node.js"
 # Install node.js
 # installs nvm (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-exec bash
+sleep 2
+source ~/.bashrc
+sleep 2
+exit
