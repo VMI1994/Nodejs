dda0394e98894e55f54b6e8fbbd086419d01c415
Just Playin <ensminger@yahoo.com>
2024-07-02 18:34:29 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,24 +6,36 @@ read version
 echo
 echo "We will now install node.js"
 
+
+# install dependencies
+sudo apt update 
+which curl
+if [ $? != 0 ]
+  sudo apt install -y curl
+fi
+
 # installs nvm (Node Version Manager)
 clear
 echo "Installing Node Version Manager"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 sleep 2
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-sleep 2
 source ~/.bashrc
-sleep 1
+sleep 2
 
 # Install node.js
 # download and install Node.js (you may need to restart the terminal)
 clear
 echo "Installing NodeJS version $version"
+sleep 2
 nvm install $version
+source ~/.bashrc
 
+
+
+#Install PM2
 clear
 echo "PM2 Process manager will now be installed"
 sleep 2
