dc3be9a97d7555269070b9f29220afac148d0145
Just Playin <ensminger@yahoo.com>
2024-08-05 10:15:38 -0400

    Create step-2.sh

---
+++ step-2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install node.js
+# download and install Node.js (you may need to restart the terminal)
+clear
+echo "Installing NodeJS version $version"
+sleep 2
+nvm install 22
+
+#Install PM2
+clear
+echo "PM2 Process manager will now be installed"
+sleep 2
+npm install pm2@latest -g
+sleep 2
+cmd=$(pm2 startup | grep sudo)
+echo $cmd | bash
+exit
