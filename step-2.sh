813a55806ea190f655f8cce3506804b0160745dc
Just Playin <ensminger@yahoo.com>
2024-08-05 10:34:37 -0400

    Update step-2.sh

--- step-2.sh
+++ step-2.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# Install node.js
-# download and install Node.js (you may need to restart the terminal)
-clear
-echo "Installing NodeJS version 22"
-sleep 2
 nvm install 22
-
 #Install PM2
 clear
 echo "PM2 Process manager will now be installed"
