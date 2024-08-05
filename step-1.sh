abc9d5c663ad069c9bc0039c8b25b85dbd10c23c
Just Playin <ensminger@yahoo.com>
2024-08-05 10:15:01 -0400

    Update and rename Install.sh to step-1.sh

---
+++ step-1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# install dependencies
+sudo apt update 
+which curl
+if [ $? != 0 ]
+then
+  sudo apt install -y curl
+fi
+sudo apt install npm
+
+# installs nvm (Node Version Manager)
+clear
+echo "Installing Node Version Manager"
+sleep 2
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+clear
+echo "A reboot is needed.  After reboot run step-2.sh"
+sleep 5
+sudo reboot now
