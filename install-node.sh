68fe4c1ff98eb532b3fe39f957d289af4ce11d2d
Just Playin <ensminger@yahoo.com>
2025-07-01 17:46:08 -0400

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -17,7 +17,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 
 # Download and install Node.js:
-nvm install 24
+nvm install 22
 
 # Verify the Node.js version:
 node -v # Should print "v24.2.0".
