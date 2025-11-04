a46ec6616e711bacd74a7887baf5df8dfd371d93
Just Playin <ensminger@yahoo.com>
2025-11-04 17:31:42 -0500

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -15,7 +15,9 @@ sleep 2
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 # in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Download and install Node.js:
 nvm install 24
